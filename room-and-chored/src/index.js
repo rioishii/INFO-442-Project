f5b11b31d39168c85861e879f83a45f01b914c5f
rioishii <rioishii@uw.edu>
2019-05-21 16:04:56 -0700

    setup firebase

--- room-and-chored/src/index.js
+++ room-and-chored/src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCCNFxIPBTcYB3UoC0oXcc1femR_vCjrNE",
+    authDomain: "roomnchored.firebaseapp.com",
+    databaseURL: "https://roomnchored.firebaseio.com",
+    projectId: "roomnchored",
+    storageBucket: "roomnchored.appspot.com",
+    messagingSenderId: "148134115781",
+    appId: "1:148134115781:web:40f830dc7d8eaeb7"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
