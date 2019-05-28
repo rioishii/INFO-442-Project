29ae194d87671a369b3813538eb4931dcc0e8d35
rioishii <rioishii@uw.edu>
2019-05-28 13:32:18 -0700

    connected redux to firebase

--- room-n-chored/src/store/actions/choreActions.js
+++ room-n-chored/src/store/actions/choreActions.js
@@ -1,5 +1,5 @@
 export const createChore = (chore) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
         dispatch({ type: 'CREATE_CHORE', chore});
     }
