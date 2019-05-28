cdd6d719de8cd96b75afe0117cab1e58d22e0c46
rioishii <rioishii@uw.edu>
2019-05-28 11:47:42 -0700

    added firebase config

---
+++ room-n-chored/src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCCNFxIPBTcYB3UoC0oXcc1femR_vCjrNE",
+    authDomain: "roomnchored.firebaseapp.com",
+    databaseURL: "https://roomnchored.firebaseio.com",
+    projectId: "roomnchored",
+    storageBucket: "roomnchored.appspot.com",
+    messagingSenderId: "148134115781",
+    appId: "1:148134115781:web:40f830dc7d8eaeb7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
