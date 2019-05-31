2a604891ee994ffb732056e74a5b5adb8cfe73c8
Bradley Balansay <bradleybalansay@gmail.com>
2019-05-30 17:10:48 -0700

    Added style to signin page

--- room-n-chored/src/components/auth/SignIn.js
+++ room-n-chored/src/components/auth/SignIn.js
@@ -26,24 +26,30 @@ class SignIn extends Component {
 
         return (
             <div>
-                <div className="container">
-                    <form onSubmit={this.handleSubmit} className="white">
-                        <h5 className="grey-text text-darken-3">Sign In</h5>
-                        <div className="input-field">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" onChange={this.handleChange}/>
-                        </div>
-                        <div className="input-field">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" onChange={this.handleChange}/>
-                        </div>
-                        <div className="input-field">
-                            <button className="btn pink lighten-1 z-depth-0">Login</button>
-                            <div className="red-text center">
-                                { authError ? <p>{authError}</p> : null}
+                <div className="container flex-container">
+                    <div>
+                        <h6>fdsgd</h6>
+                        <p>sdkjghf</p>
+                    </div>
+                    <div>
+                        <form onSubmit={this.handleSubmit}>
+                            <h5 className="grey-text text-darken-3">Sign In</h5>
+                            <div className="input-field">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" id="email" onChange={this.handleChange}/>
                             </div>
-                        </div>
-                    </form>
+                            <div className="input-field">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" id="password" onChange={this.handleChange}/>
+                            </div>
+                            <div className="input-field">
+                                <button className="btn pink lighten-1 z-depth-0">Login</button>
+                                <div className="red-text center">
+                                    { authError ? <p>{authError}</p> : null}
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         )
