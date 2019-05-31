192da96a52909c8a6dd3ca6753f8c78cfdb9a5f2
Benny Nguyen <benny.nguyenx@gmail.com>
2019-05-30 17:43:34 -0700

    Add ability to delete chores

--- room-n-chored/src/components/chores/ChoreSummary.js
+++ room-n-chored/src/components/chores/ChoreSummary.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import moment from 'moment';
+import { getFirestore } from 'redux-firestore';
 
 const ChoreSummary = ({chore}) => {
+    const firestore = getFirestore();
     return (
         <div className="card z-depth chore-summary">
+            <button id="deleteChoreBtn" onClick={choreId => firestore.collection('chores').doc(chore.id).delete()}>&times;</button>
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{chore.title}</span>
                 <p>Posted by {chore.authorFirstName} {chore.authorLastName}</p>
