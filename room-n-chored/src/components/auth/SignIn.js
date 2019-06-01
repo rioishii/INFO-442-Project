85f015c5aa9e680a25e3c561af77accb600c2fb2
Bradley Balansay <bradleybalansay@gmail.com>
2019-06-01 16:19:08 -0700

    Styled sign-in page

--- room-n-chored/src/components/auth/SignIn.js
+++ room-n-chored/src/components/auth/SignIn.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { signIn } from '../../store/actions/authActions';
 import { Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class SignIn extends Component {
     state = {
@@ -28,12 +29,22 @@ class SignIn extends Component {
             <div>
                 <div className="container flex-container">
                     <div>
-                        <h6>fdsgd</h6>
-                        <p>sdkjghf</p>
+                        <h3 style={{margin: '0px 10px 0px 5px', whiteSpace: 'nowrap'}}>Room & Chored</h3>
+                        <p style={{margin: '0px'}}>Making sure everyone does their fair share.</p>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <p style={{margin: '0px'}}>Don't have an account?</p>
+                        <NavLink to='/signup' style={{width: '40%'}}>
+                            <button className="btn lighten-1 z-depth-0" style={{color: 'rgb(25, 96, 140)', backgroundColor: 'rgb(255, 249, 239)', width: '100%'}}>
+                                Sign Up
+                            </button>
+                        </NavLink>
                     </div>
                     <div>
                         <form onSubmit={this.handleSubmit}>
-                            <h5 className="grey-text text-darken-3">Sign In</h5>
+                            <h4>Welcome!</h4>
+                            <br></br>
                             <div className="input-field">
                                 <label htmlFor="email">Email</label>
                                 <input type="email" id="email" onChange={this.handleChange}/>
@@ -43,7 +54,9 @@ class SignIn extends Component {
                                 <input type="password" id="password" onChange={this.handleChange}/>
                             </div>
                             <div className="input-field">
-                                <button className="btn pink lighten-1 z-depth-0">Login</button>
+                                <button className="btn lighten-1 z-depth-0" style={{color: 'rgb(255, 249, 239)', backgroundColor: 'rgb(25, 96, 140)', width: '100%'}}>
+                                    Sign In
+                                </button>
                                 <div className="red-text center">
                                     { authError ? <p>{authError}</p> : null}
                                 </div>
